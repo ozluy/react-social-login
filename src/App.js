f4fc3515e11e1ddd306e12510899926f33073d3e
Yusuf Ozlu <ozluyusuf@gmail.com>
2018-05-16 13:53:21 +0200

    connect facebook login to app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+import enhancers from './enhancers'
+import Button from './components/Button'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
+const App = ({ loginWithFacebook, loginedFacebook }) => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Social Login</h1>
+    </header>
+    <main>
+      <Button success={loginedFacebook} onClick={loginWithFacebook}>
+        Login with Facebook
+      </Button>
+    </main>
+  </div>
+)
+
+App.propTypes = {
+  loginWithFacebook: PropTypes.func.isRequired,
+  loginedFacebook: PropTypes.bool.isRequired,
 }
 
-export default App
+export default enhancers(App)
