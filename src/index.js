5ae5613de45a6bc16310de366290bde9e0ca8275
Yusuf Ozlu <ozluyusuf@gmail.com>
2018-05-17 23:29:58 +0200

    add routing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './globalStyles'
-import App from './App'
+import AppRouter from './router'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<AppRouter />, document.getElementById('root'))
 registerServiceWorker()
