25d12377f2bd995d4837936c1b36547622d307ad
Yusuf Ozlu <ozluyusuf@gmail.com>
2018-05-17 23:27:20 +0200

    update button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,3 +1,21 @@
-import Button from './styled'
+import React from 'react'
+import { Button, Text, Overlay } from './styled'
+import PropTypes from 'prop-types'
 
-export default Button
+const StyledButton = props => (
+  <Button secondary={props.secondary} {...props}>
+    <Overlay />
+    <Text>{props.label}</Text>
+  </Button>
+)
+
+StyledButton.defaultProps = {
+  secondary: false,
+}
+
+StyledButton.propTypes = {
+  label: PropTypes.string.isRequired,
+  secondary: PropTypes.bool,
+}
+
+export default StyledButton
--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -1,33 +1,95 @@
 import styled, { css } from 'styled-components'
-import Tick from './icon/tick.svg'
+import variables from 'common/styleVariables'
 
-export default styled.button`
-  padding: 15px 30px;
+export const Overlay = styled.span`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width: 140%;
+  transform: translate3d(100%, 0, 0) skew(0deg);
+  background-color: #c30928;
+  opacity: 0;
+  transition: transform 0.5s ease, opacity 0.3s ease;
   border-radius: 2px;
-  outline: none;
-  border: none;
-  width: 300px;
-  position: relative;
-  background-color: #999;
-  color: #fff;
-  cursor: pointer;
   display: block;
-  font-size: 16px;
-  margin: 20px auto;
-  &:hover {
-    background-color: #888;
+`
+export const Text = styled.span`
+  transition: opacity 0.25s ease, transform 0.25s ease;
+  z-index: 1;
+  display: inline-block;
+`
+
+export const Button = styled.button`
+  margin: 40px 0;
+  overflow: hidden;
+  color: ${variables.colors.white};
+  display: block;
+  cursor: pointer;
+  font-family: ${variables.fonts.primary};
+  font-weight: 700;
+  position: relative;
+  font-size: 1.3rem;
+  letter-spacing: 2px;
+  text-decoration: none;
+  text-align: center;
+  text-transform: uppercase;
+  outline: none;
+  border: 2px solid transparent;
+  border-radius: 2px;
+  transition: background-color 0.3s, color 0.3s, border-color 0.3s;
+  background-color: ${variables.colors.primary};
+  width: 100%;
+  max-width: 100%;
+  padding: 15px 0 16px;
+  &:before {
+    content: attr(data-text);
+    position: absolute;
+    transform: translate3d(50%, 0, 0);
+    opacity: 0;
+    transition: opacity 0.25s ease, transform 0.25s ease;
+    z-index: 1;
+    display: inline-block;
   }
-  ${({ success }) =>
-    success &&
+  &:hover {
+    &:before {
+      transform: translateZ(0);
+      opacity: 1;
+    }
+    ${Text} {
+      transform: translate3d(-50%, 0, 0);
+      opacity: 0;
+    }
+    ${Overlay} {
+      transform: translate3d(10%, 0, 0) skew(-20deg);
+      opacity: 1;
+    }
+  }
+  ${({ secondary }) =>
+    secondary &&
     css`
-      &:after {
-        background-image: url(${Tick});
-        position: absolute;
-        content: '';
-        width: 25px;
-        height: 25px;
-        right: 10px;
-        top: 10px;
+      background-color: transparent;
+      &:before {
+        color: ${variables.colors.grey};
+      }
+      ${Overlay} {
+        background-color: transparent;
+      }
+      ${Text} {
+        color: ${variables.colors.grey};
+      }
+      &:hover {
+        ${Text} {
+          z-index: 1;
+          display: inline-block;
+          transform: translate3d(0, 0, 0);
+          opacity: 1;
+          color: ${variables.colors.black};
+        }
+        &:before {
+          transform: translate3d(50%, 0, 0);
+          opacity: 0;
+        }
       }
     `};
 `
