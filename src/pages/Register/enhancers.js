6ec2fcc965fea8cd14f5f324b6ea021c2b197ba3
Yusuf Ozlu <ozluyusuf@gmail.com>
2018-05-21 16:38:40 +0200

    add loading and image functionality

--- src/pages/Register/enhancers.js
+++ src/pages/Register/enhancers.js
@@ -1,4 +1,4 @@
-import { compose, withHandlers } from 'recompose'
+import { compose, withHandlers, withState } from 'recompose'
 import facebookClient from 'api/facebookClient'
 import twitterClient from 'api/twitterClient'
 import googleClient from 'api/googleClient'
@@ -9,8 +9,10 @@ import { withRegister } from 'common/registerContextAPI'
 
 export default compose(
   withRegister,
+  withState('loading', 'setLoading', false),
   withHandlers({
-    loginWithFacebook: ({ register }) => async () => {
+    loginWithFacebook: ({ register, setLoading }) => async () => {
+      setLoading(true)
       const loginRequest = await facebookClient.login()
       if (loginRequest.success) {
         const facebookData = loginRequest.data
@@ -18,14 +20,17 @@ export default compose(
           email: facebookData.additionalUserInfo.profile.email,
           firstName: facebookData.additionalUserInfo.profile.first_name,
           lastName: facebookData.additionalUserInfo.profile.last_name,
+          picture: facebookData.additionalUserInfo.profile.picture.data.url,
           accessToken: facebookData.credential.accessToken,
           providerId: facebookData.credential.providerId,
         }
+        setLoading(false)
         register.setRegisterData(dataForRegister)
         history.push('/complete-register')
       }
     },
-    loginWithTwitter: ({ register }) => async () => {
+    loginWithTwitter: ({ register, setLoading }) => async () => {
+      setLoading(true)
       const loginRequest = await twitterClient.login()
       if (loginRequest.success) {
         const twitterData = loginRequest.data
@@ -34,24 +39,30 @@ export default compose(
           email: twitterData.additionalUserInfo.profile.email,
           firstName: fullName[0],
           lastName: fullName[1],
+          picture:
+            twitterData.additionalUserInfo.profile.profile_image_url_https,
           accessToken: twitterData.credential.accessToken,
           providerId: twitterData.credential.providerId,
         }
+        setLoading(false)
         register.setRegisterData(dataForRegister)
         history.push('/complete-register')
       }
     },
-    loginWithGoogle: ({ register }) => async () => {
+    loginWithGoogle: ({ register, setLoading }) => async () => {
+      setLoading(true)
       const loginRequest = await googleClient.login()
       if (loginRequest.success) {
         const googleData = loginRequest.data
         const dataForRegister = {
           email: googleData.additionalUserInfo.profile.email,
           firstName: googleData.additionalUserInfo.profile.given_name,
+          picture: googleData.additionalUserInfo.profile.picture,
           lastName: googleData.additionalUserInfo.profile.family_name,
           accessToken: googleData.credential.accessToken,
           providerId: googleData.credential.providerId,
         }
+        setLoading(false)
         register.setRegisterData(dataForRegister)
         history.push('/complete-register')
       }
@@ -59,7 +70,8 @@ export default compose(
     loginWithInstagram: () => () => {
       instagramClient.login()
     },
-    loginWithLinkedIn: ({ register }) => async () => {
+    loginWithLinkedIn: ({ register, setLoading }) => async () => {
+      setLoading(true)
       const loginRequest = await linkedInClient.login()
       if (loginRequest.success) {
         const linkedInData = loginRequest.data
@@ -67,9 +79,11 @@ export default compose(
           email: linkedInData.emailAddress,
           firstName: linkedInData.firstName,
           lastName: linkedInData.lastName,
+          picture: linkedInData.pictureUrl,
           accessToken: linkedInData.id,
           providerId: 'linkedin.com',
         }
+        setLoading(false)
         register.setRegisterData(dataForRegister)
         history.push('/complete-register')
       }
