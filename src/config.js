deab84792607f419f92309a8f0ea7c30066a396e
Yusuf Özlü <ozluyusuf@gmail.com>
2018-05-24 15:54:25 +0200

    force for https

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
     redirectUri:
       process.env.NODE_ENV === 'production' // eslint-disable-line
         ? 'https://{FIREBASE_PROJECT_ID}.firebaseapp.com/complete-register/'
-        : 'http://localhost:3000/complete-register/',
+        : 'https://localhost:3000/complete-register/',
     responseType: 'token',
   },
 }
